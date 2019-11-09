23a918df531cf32f36908d35ed9b82e5e5f27e69
LR <lr177245@gmail.com>
2019-11-08 20:55:10 -0800

    growing your own...

--- my_framework/hello_world.rb
+++ my_framework/hello_world.rb
@@ -1,7 +1,32 @@
 # hello_world.rb
 
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ["Hello World!"]]
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {"Content-Type" => 'text/html'}, [erb(:index)] ]
+    when '/advice'
+      some_advice = Advice.new.generate
+      ['200', {"Content-Type" => 'text/html'}, ["<html><body><b><em>#{some_advice}</em></b></body></html>"]]
+    else
+      not_found_text = "<html><body><h4>404 Not Found</h4></body></html>"
+      [
+        '404',
+        {"Content-Type" => 'text/html', "Content-Length" => "#{not_found_text.length}"},
+        ["#{not_found_text}"]
+      ]
+    end
+  end
+  #def call(env)
+  #  ['200', {'Content-Type' => 'text/plain'}, ["Hello World!"]]
+  #end
+
+  private
+
+  def erb(file_name)
+    template = File.read("views/#{file_name}.erb")
+    ERB.new(template).result
   end
 end
