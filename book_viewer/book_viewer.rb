d0d53a0b0c79a605464a18f5b0668c157dfd5565
LR <lr177245@gmail.com>
2019-11-18 20:57:18 -0800

    doing book viewer

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,6 +1,17 @@
+require 'tilt/erubis'
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  #@toc = File.read("data/toc.txt").split("\n")
+  @toc = File.readlines("data/toc.txt")
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @toc = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+  erb :chapter
 end
