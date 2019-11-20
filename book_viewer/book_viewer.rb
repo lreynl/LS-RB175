67d6c219f91c32f196758a4a4eda617a2df2af86
lreynl <lr177245@gmail.com>
2019-11-19 21:45:22 -0800

    doing book viewer

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,16 +2,17 @@ require 'tilt/erubis'
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  #@toc = File.read("data/toc.txt").split("\n")
-  @toc = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/:number" do
   number = params[:number]
-  @toc = File.readlines("data/toc.txt")
   @title = "Chapter #{number}: #{@toc[number.to_i - 1]}"
   @chapter = File.read("data/chp#{number}.txt")
   erb :chapter
