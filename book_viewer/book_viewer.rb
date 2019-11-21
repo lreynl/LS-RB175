b7926a0fb17998252d69b2285459530a071b7d93
lreynl <lr177245@gmail.com>
2019-11-20 21:29:07 -0800

    doing book viewer search

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -6,14 +6,37 @@ before do
   @toc = File.readlines("data/toc.txt")
 end
 
+helpers do
+  def in_paragraphs(txt)
+    txt = txt.split("\n\n")
+    txt.map { |paragraph| "<p>#{paragraph}</p>" }.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   erb :home
 end
 
 get "/chapters/:number" do
-  number = params[:number]
-  @title = "Chapter #{number}: #{@toc[number.to_i - 1]}"
+  number = params[:number].to_i
+  redirect "/" unless (1..@toc.length).cover? number
+  @title = "Chapter #{number}: #{@toc[number - 1]}"
   @chapter = File.read("data/chp#{number}.txt")
   erb :chapter
+end
+
+get "/search/:query" do
+  query = params[:query]
+  search_results = []
+  (1..@toc.length).to_a.each do |chapter_number|
+    chapter = File.read("data/chp#{number}.txt")
+    search_results << @toc[chapter_number - 1] if chapter.include?(query)
+  end
+  
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
