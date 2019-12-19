64f6828e48f13df94ce326e64aa511a057a020c5
lreynl <lr177245@gmail.com>
2019-12-19 12:24:25 -0800

    doing file based cms

--- cms/cms.rb
+++ cms/cms.rb
@@ -13,36 +13,49 @@ helpers do
   end
 end
 
+def files_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/files", __FILE__)
+  else
+    File.expand_path("../files", __FILE__)
+  end
+end
+
 get "/" do
+  path = files_path + "*.*"
   @files = Dir["./files/*.*"]
   @files.map! { |file| file.split("/")[-1] }
   erb :file_list#, layout: :layout
 end
 
 get "/:filename" do
-  @name = "files/" + params[:filename]
+  #@name = "files/" + params[:filename]
+  #@name = File.join(files_path, params[:filename])
+  @name = File.join(files_path, params[:filename])
   if File.exist?(@name)
     erb :view_text #, layout: :layout
   else
     session[:not_found] = "#{params[:filename]} not found."
     redirect "/"
+    #erb @name
   end
 end
 
 get "/edit/:filename" do
   @filename = params[:filename]
-  @text = File.read("files/" + @filename)
+  path = File.join(files_path, @filename)
+  @text = File.read(path)
   erb :edit
 end
 
 post "/:filename" do
-  filename = params[:filename]
-  path = "files/" + filename
+  filename = params[:filename] || params[:content]
+  path = File.join(files_path, filename)
   text = params[:text_box]
-  File.open(path, "w") { |file| 
-  file.puts(text) 
-  file.close
-  }
+  File.open(path, "w") do |file| 
+    file.puts(text) 
+    file.close
+  end
   session[:success] = filename + " was updated."
   redirect "/"
 end
--- cms/test/app_test.rb
+++ cms/test/app_test.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test"
 require "minitest/autorun"
 require "rack/test"
 require "erubis"
+require "fileutils"
 
 require_relative "../cms"
 
@@ -13,7 +14,22 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(files_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(files_path)
+  end
+
+  def create_document(name, text = "")
+    File.open(File.join(files_path, name), 'w') { |file| file.write(text) }
+  end
+
   def test_index
+    create_document "example.md"
+    create_document "about.txt"
+    
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
@@ -23,6 +39,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_history
+    create_document "history.txt", "1993 - Yukihiro Matsumoto dreams up Ruby."
     get "/history.txt"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
@@ -40,6 +57,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_md
+    create_document "example.md", "<h1>Heading</h1><h2>Sub-heading</h2><code>monospace</code>"
     get "/example.md"
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<h1>Heading</h1>"
@@ -48,20 +66,25 @@ class CMSTest < Minitest::Test
   end
 
   def test_edit
+    create_document "about.txt", '<textarea name='
     get "/edit/about.txt"
     assert_equal 200, last_response.status
     assert_includes last_response.body, '<textarea name='
   end
 
-  def test_update
-    post "/about.txt", content: "new content"
-    assert_equal 302, last_response.status
-    
-    get last_response["Location"]
-    assert_includes last_response.body, "about.txt was updated"
 
-    #get "/about.txt"
-    #assert_equal 200, last_response.status
-    #assert_includes last_response.body, "new content"
+  def test_updating_document
+    create_document "changes.txt"
+    post "/changes.txt", content: "new content"
+  
+    assert_equal 302, last_response.status
+  
+    get last_response["Location"]
+      
+    assert_includes last_response.body, "changes.txt was updated"
+      
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
   end
 end
